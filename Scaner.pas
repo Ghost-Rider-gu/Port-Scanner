731ea52c920e566afe7aec3935a157157a909ab0
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2016-04-21 21:16:57 +0300

    update scaner

--- Scaner.pas
+++ Scaner.pas
@@ -50,20 +50,20 @@ type
         procedure ToolButton5Click(Sender: TObject);
         procedure ToolButton9Click(Sender: TObject);
     private
-        procedure ShowMyHint(Sender: TObject); //обработка хинтов
-        function  GetNetWorkConnect: boolean; //проверка локальной сети
-        function  GetINetConnect: boolean; //проверка интернета
-        function  LookupName(myhost:string): TInAddr; //проверка хоста на корректность
-        procedure Scan(MinPort:string; MaxPort:string; Host:string); //сканирование сетевых портов
+        procedure ShowMyHint(Sender: TObject); // my hint
+        function  GetNetWorkConnect: boolean; // check network
+        function  GetINetConnect: boolean; // check internet
+        function  LookupName(myhost:string): TInAddr; // check host
+        procedure Scan(MinPort:string; MaxPort:string; Host:string); // port scaning
     public
 
     end;
 
 const
-    NoneConnect     = 'Подключений не обнаружено';
-    LocalConnect    = 'Обнаружено подключение к локальной сети';
-    NetConnect      = 'Обнаружено подключение к Интернету';
-    NetLocalConnect = 'Обнаружено подключение к локальной сети и Интернету';
+    NoneConnect     = 'Not connect';
+    LocalConnect    = 'Local Network connect';
+    NetConnect      = 'Internet connect';
+    NetLocalConnect = 'Local Network and Internet';
 
 var
     Form1: TForm1;
@@ -74,7 +74,7 @@ implementation
 
 {$R *.dfm}
 
-//проверяем поключены ли к интернету
+// Check internet connect
 function TForm1.GetINetConnect: boolean;
 var
     dwConnectionTypes: DWORD;
@@ -83,16 +83,16 @@ begin
     Result := InternetGetConnectedState(@dwConnectionTypes, 0);
 end;
 
-//проверяем подключены ли к локальной сети
+// Check Local Network connect
 function TForm1.GetNetWorkConnect: boolean;
 begin
     if (GetSystemMetrics(SM_NETWORK)) AND ($01 = $01) then
-        Result:= True
+        Result := True
     else
-        Result:= False;
+        Result := False;
 end;
 
-//преобразование в IP адрес
+// Convert IP address
 function TForm1.LookupName(myhost: string): TInAddr;
 var
     HostEnt: PHostEnt;
@@ -105,7 +105,7 @@ begin
     else
         begin
             HostEnt := gethostbyname(PChar(myhost));
-            Error:=GetLastError;
+            Error := GetLastError;
             if Error = 0 then
                 begin
                     Str := HostEnt^.h_addr_list^;
@@ -118,7 +118,7 @@ begin
     Result := InAddr;
 end;
 
-//сканирование сетевых портов
+// Scaning
 procedure TForm1.Scan(MinPort, MaxPort, Host: string);
 var
     i,j,s,opt,index:integer;
@@ -142,7 +142,7 @@ begin
     addr.sin_addr := LookupName(Host);
 
     DataScan.SelAttributes.Color := clRed;
-    DataScan.Lines.Add('СКАНИРОВАНИЕ . . .');
+    DataScan.Lines.Add('SCANING . . .');
 
     index := 40;
 
@@ -158,179 +158,114 @@ begin
                 index := j-1;
                 break;
             end;
-            FSocket[j]:=socket(AF_INET,SOCK_STREAM,IPPROTO_IP);
-            WSAEventSelect(FSocket[j],hEvent, FD_WRITE+FD_CONNECT);
-            addr.sin_port:=htons(i);
+
+            FSocket[j] := socket(AF_INET,SOCK_STREAM,IPPROTO_IP);
+            WSAEventSelect(FSocket[j], hEvent, FD_WRITE + FD_CONNECT);
+            addr.sin_port := htons(i);
             connect(FSocket[j], @addr, sizeof(addr));
             Application.ProcessMessages;
-            if WSAGetLastError()=WSAEINPROGRESS then
+
+            if WSAGetLastError() = WSAEINPROGRESS then
             begin
-                closesocket (FSocket[j]);
-                busy[j]:=true;
+                closesocket(FSocket[j]);
+                busy[j] := true;
             end;
-            port[j]:=i;
-            i:=i+1;
+            port[j] := i;
+            i := i + 1;
         end;
+
         FD_Zero(fset);
+
         for j := 0 to index do
         begin
             if busy[j] <> true then
-                FD_SET (FSocket[j], fset);
+                FD_SET(FSocket[j], fset);
         end;
         Application.ProcessMessages;
         tv.tv_sec := 1;
         tv.tv_usec := 0;
-        s:=select (I, nil, @fset, nil, @tv);
+        s := select (I, nil, @fset, nil, @tv);
         Application.ProcessMessages;
         for j := 0 to index do
         begin
             if busy[j] then continue;
             if FD_ISSET (FSocket[j], fset) then
             begin
-                s:=Sizeof(Opt);
-                opt:=1;
+                s := Sizeof(Opt);
+                opt := 1;
                 getsockopt(FSocket[j], SOL_SOCKET, SO_ERROR, @opt, s);
-                if opt=0 then
+                if opt = 0 then
                 begin
-                    tec := getservbyport(htons(Port[j]),'TCP');
-                    if tec=nil then
-                        PName:='Unknown'
+                    tec := getservbyport(htons(Port[j]), 'TCP');
+                    if tec = nil then
+                        PName := 'Unknown'
                     else
                     begin
-                        PName:=tec.s_name;
+                        PName := tec.s_name;
                     end;
                     Media.Play;
-                    DataScan.Lines.Add('Хост:'+CompName.Text+': порт :'+IntToStr(Port[j])+' '+Pname+' '+' открыт ') ;
+                    DataScan.Lines.Add('Host:' + CompName.Text + ': port :' + IntToStr(Port[j]) + ' ' + Pname + ' ' + ' open ') ;
                 end;
             end;
             closesocket(FSocket[j]);
         end;
     end;
     WSACloseEvent(hEvent);
-    DataScan.SelAttributes.Color:=clRed;
-    DataScan.Lines.Add ('СКАНИРОВАНИЕ ЗАВЕРШЕНО . . .');
+    DataScan.SelAttributes.Color := clRed;
+    DataScan.Lines.Add ('SCANING DONE . . .');
 
 end;
 
-//показываем в статус баре хинты программы
+// Show hints in status bar
 procedure TForm1.ShowMyHint(Sender: TObject);
 begin
-    StatBar.Panels[1].Text:=Application.Hint;
+    StatBar.Panels[1].Text := Application.Hint;
 end;
 
-//установка начальных данных и другие настройки программы
+// Init
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-    Application.OnHint:=ShowMyHint;
+    Application.OnHint := ShowMyHint;
 
-    Screen.Cursors[MyCursor]:=LoadCursorFromFile('resources\Cursor.ani');
-    Form1.Cursor:=MyCursor;
-    DataScan.Cursor:=MyCursor;
-    StatBar.Cursor:=MyCursor;
-    ToolBar1.Cursor:=MyCursor;
-    Button1.Cursor:=MyCursor;
-    Label2.Cursor:=MyCursor;
-    Label3.Cursor:=MyCursor;
-    Label4.Cursor:=MyCursor;
-    Info.Cursor:=MyCursor;
-    StartPort.Cursor:=MyCursor;
-    EndPort.Cursor:=MyCursor;
-    CompName.Cursor:=MyCursor;
+    Screen.Cursors[MyCursor] := LoadCursorFromFile('resources\Cursor.ani');
+    Form1.Cursor             := MyCursor;
+    DataScan.Cursor          := MyCursor;
+    StatBar.Cursor           := MyCursor;
+    ToolBar1.Cursor          := MyCursor;
+    Button1.Cursor           := MyCursor;
+    Label2.Cursor            := MyCursor;
+    Label3.Cursor            := MyCursor;
+    Label4.Cursor            := MyCursor;
+    Info.Cursor              := MyCursor;
+    StartPort.Cursor         := MyCursor;
+    EndPort.Cursor           := MyCursor;
+    CompName.Cursor          := MyCursor;
 
     GetDir(0,Path);
-    Media.FileName:=Path+'\resources\openport.wav';
+    Media.FileName := Path+'\resources\openport.wav';
 
     if (GetINetConnect) and (GetNetWorkConnect) then
     begin
-        info.Caption:=NetLocalConnect;
-        StatBar.Panels[0].Text:='Вы можете сканировать порты удаленного компьютера';
+        info.Caption := NetLocalConnect;
+        StatBar.Panels[0].Text := 'Scan ports remote computer';
     end;
     if (not GetINetConnect) and (not GetNetWorkConnect) then
     begin
-        info.Caption:=NoneConnect;
-        StatBar.Panels[0].Text:='Вы можете сканировать только порты локального компьютера';
-    end;
-    if (GetINetConnect) and (not GetNetWorkConnect) then
-    begin
-        info.Caption:=NetConnect;
-        StatBar.Panels[0].Text:='Вы можете сканировать порты удаленного компьютера';
-    end;
-    if (not GetINetConnect) and (GetNetWorkConnect) then
-    begin
-        info.Caption:=LocalConnect;
-        StatBar.Panels[0].Text:='Вы можете сканировать порты компьютера локальной сети';
+        info.Caption := NoneConnect;
+        StatBar.Panels[0].Text := 'Scan ports only local computer';
     end;
 end;
 
-//Закрываем приложение
-procedure TForm1.ToolButton8Click(Sender: TObject);
-begin
-    Application.Terminate;
-end;
-
-//Сканирование заданных портов
+// Select ports
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-    if (startport.Text='') and (endport.Text='') and (compname.Text='') then
+    if (startport.Text = '') and (endport.Text = '') and (compname.Text = '') then
     begin
-        ShowMessage('Не заполнено одно из полей. Пожайлуста заполните все поля перед сканированием');
+        ShowMessage('Empty field. Please input ports and host');
         Exit;
     end;
 
     Scan(startport.Text, endport.Text, CompName.Text);
-end;
-
-//Отправить письмо автору
-procedure TForm1.ToolButton6Click(Sender: TObject);
-begin
-    ShellExecute(Handle,'OPEN','MailTo:ghostrider.gu@gmail.com',nil,nil,SW_SHOWNORMAL);
-end;
-
-//Открыть ранее сохраненный документ
-procedure TForm1.ToolButton1Click(Sender: TObject);
-begin
-    if OpenDig.Execute then
-    begin
-        DataScan.Lines.LoadFromFile(OpenDig.FileName);
-        FFileName:=OpenDig.FileName;
-    end;
-end;
-
-//Сохранить документ
-procedure TForm1.ToolButton2Click(Sender: TObject);
-begin
-    if SaveDig.Execute then
-    begin
-        DataScan.Lines.SaveToFile(SaveDig.FileName);
-        FFileName:=SaveDig.FileName;
-    end;
-end;
-
-//Распечатать документ
-procedure TForm1.ToolButton3Click(Sender: TObject);
-begin
-    if FFileName='' then
-    begin
-        ShowMessage('Сохраните Ваши данные перед распечаткой');
-        Exit;
-    end;
-
-    if PrintDig.Execute then
-    begin
-        DataScan.Print(FFileName);
-    end;
-end;
-
-//О программе
-procedure TForm1.ToolButton5Click(Sender: TObject);
-begin
-    Form2.ShowModal;
-end;
-
-//Окошко HelpManual
-procedure TForm1.ToolButton9Click(Sender: TObject);
-begin
-    Form3.Show;
 end;
 
 end.
